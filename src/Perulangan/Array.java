b46cf22e5789505078bdc73735d203b9f2d622ea
AdeSetyaWijaya00 <AdeSetyaWijaya00@gmail.com>
2020-04-21 14:26:00 +0800

    Array

---
+++ src/Perulangan/Array.java
@@ -0,0 +1,11 @@
+package Perulangan;
+
+public class Array {
+    public static void main(String[] args) {
+
+        int nilaiArr[] = {12,54,65,76,87,98,90,43,54};
+        System.out.println(nilaiArr.length);
+        System.out.println(nilaiArr[0]);
+        System.out.println(nilaiArr[8]);
+    }
+}
