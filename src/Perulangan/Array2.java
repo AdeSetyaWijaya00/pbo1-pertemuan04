b31b37628d68b7d067745d3a73fd43a76c8bfa16
AdeSetyaWijaya00 <AdeSetyaWijaya00@gmail.com>
2020-04-21 14:30:03 +0800

    Array2

--- src/Perulangan/Array2.java
+++ src/Perulangan/Array2.java
@@ -1,4 +1,14 @@
 package Perulangan;
 
 public class Array2 {
+    public static void main(String[] args) {
+        String matakuliah[];
+        matakuliah = new String[10];
+
+        matakuliah[0] = "Pemrograman Berbasis Objek 1";
+        matakuliah[1] = "Visual 2";
+        matakuliah[2] = "Rekayasa Perangkat Lunak";
+        matakuliah[3] = "Organisasi Komputer";
+        matakuliah[4] = "Pemrograman Web 2";
+    }
 }
